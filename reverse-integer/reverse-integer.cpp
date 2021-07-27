28044a3f2eaf71f3bf81d426c48270a3b16406ba
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-07-27 19:26:48 +0530

    Time: 0 ms (100.00%), Space: 6 MB (31.24%) - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x){
+            if(rev > INT_MAX/10 || rev < INT_MIN/10)
+                return 0;
+            rev = rev*10 + x%10;
+            x /= 10; 
+        }
+        return rev;
+    }
+};
