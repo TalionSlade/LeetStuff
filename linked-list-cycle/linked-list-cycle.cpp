a80b684a88caef526bc39f7ac09c8deea35bf44a
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-07-31 11:42:13 +0530

    Time: 8 ms (97.13%), Space: 7.8 MB (96.22%) - LeetHub

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -1,18 +1,17 @@
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head) 
-            return false;
-        
-        ListNode *slow = head, *fast = head;
-        
-        while (fast->next && fast->next->next) {
+        auto *fast = head, *slow = head;
+        if(fast == NULL || fast->next  == NULL ) return false;
+        while(fast->next!= NULL && fast->next->next != NULL ){
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast) 
+            if(slow == fast){
                 return true;
+            }
+            
         }
-        
         return false;
     }
 };
