71250622af576ad446be99af2fa16a21a67b41c9
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-08-03 18:00:33 +0530

    Time: 4 ms (79.57%), Space: 6.6 MB (95.03%) - LeetHub

---
+++ remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        int count = 0;
+        std::string str;
+        for (char c : S) {
+            if (c == '(') {
+                if (count++) {
+                    str += '(';
+                }
+            } else {
+                if (--count) {
+                    str += ')';
+                }
+            }
+        }
+        return str;
+    }
+};
