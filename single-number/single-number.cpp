7766c5563cdec2563d3bd1a2c1749453b5120827
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-08-04 11:58:10 +0530

    Time: 16 ms (85.14%), Space: 17 MB (51.77%) - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> table;
-        auto it = nums.begin();
-        while(it != nums.end()){
-            if(table.find(*it)==table.end())
-                table.insert(pair<int,int>(*it,*it));
-            else
-                table.erase(*it);
-            ++it;
-        }
-        return(table.begin()->first);
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+           ans = ans ^ nums[i];
+        
+        return ans;
     }
 };
