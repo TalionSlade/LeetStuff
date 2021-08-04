5d0baad865f5a7c25225d66eb610f434559fb07c
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-08-05 01:10:05 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (41.58%) - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            if(arr[i]>arr[i+1]) return i;
+        }
+        return arr.size()-1;
+    }
+};
