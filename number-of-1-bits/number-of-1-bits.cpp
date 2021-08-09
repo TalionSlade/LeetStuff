2985218f0af3693da29bc8fb9e41463c7b4efa25
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-08-09 23:50:48 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (77.51%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+		// support variables
+        int res = 0;
+        while (n) {
+            // updating res
+			res += n & 1;
+			// reducing n
+            n >>= 1;
+        }
+        return res;
+    }
+};
