e4ff42f35ee38138dfa3174ca1b1bce019df0bdd
Arpan Ghosh <53509258+TalionSlade@users.noreply.github.com>
2021-12-16 18:44:18 -0800

    Time: 232 ms (34.36%), Space: 120.8 MB (39.01%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.cpp
+++ palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,25 +11,50 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        deque<int>q;
-        ListNode* p=head;
-        while(p)
-        {
-            q.push_back(p->val);
-            p=p->next;
-        }
-        while(!q.empty())
-        {
-            if(q.front()==q.back())
-            {
-                q.pop_front();
-                if(!q.empty())
-                q.pop_back();
+        stack<int> st;
+        auto *p = head;//slow
+        auto *q = head;//fast
+        while(q){
+            if(q->next == NULL){
+                p=p->next;
+                break;
             }
-            else
-            return false;
+            st.push(p->val);
+            p = p->next;
+            q = q->next->next;
         }
-        return true;
+        
+        while(p){
+            if(p->val != st.top())return false;
+            p = p->next;
+            st.pop();
+            
+        }
+        return true;        
+        
+        
+        
+        
+        
+        // deque<int>q;
+        // ListNode* p=head;
+        // while(p)
+        // {
+        //     q.push_back(p->val);
+        //     p=p->next;
+        // }
+        // while(!q.empty())
+        // {
+        //     if(q.front()==q.back())
+        //     {
+        //         q.pop_front();
+        //         if(!q.empty())
+        //         q.pop_back();
+        //     }
+        //     else
+        //     return false;
+        // }
+        // return true;
         
     }
 };
